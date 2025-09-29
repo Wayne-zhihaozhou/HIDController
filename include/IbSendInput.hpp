3ae2ba4a3f93a3e66d89375001c9090fdd4503bb
zhihao.zhou <1559152947@qq.com>
2025-09-29 10:38:36 +0800

    HOOK.hpp文件移除

--- include/IbSendInput.hpp
+++ include/IbSendInput.hpp
@@ -20,3 +20,24 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+
+
+DLLAPI void __stdcall IbSendInputHook(HookCode code) {
+    switch (code) {
+    case HookCode::InitOnly:
+        sendinput_hook.create(); // 初始化钩子
+        break;
+    case HookCode::Destroy:
+        sendinput_hook.destroy(); // 卸载钩子
+        break;
+    case HookCode::On:
+        if (!sendinput_hook.created())
+            sendinput_hook.create();
+        sendinput_hook->hook = true; // 启用钩子
+        break;
+    case HookCode::Off:
+        sendinput_hook->hook = false; // 禁用钩子
+        break;
+    }
+}
+
