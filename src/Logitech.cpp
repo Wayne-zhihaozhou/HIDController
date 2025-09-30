e524a1edf59b7d7b8de077c9b81a35173992ba8a
zhihao.zhou <1559152947@qq.com>
2025-10-01 07:40:45 +0800

    增加注释

--- src/Logitech.cpp
+++ src/Logitech.cpp
@@ -1,23 +1,22 @@
 #include "Logitech.hpp" 
 #include <string_view>
 
-
 namespace Send::Type::Internal {
 
+	// 构造函数：初始化 VirtualKeyStates 基类
 	Logitech::Logitech() : VirtualKeyStates(keyboard_report.modifiers, keyboard_mutex) {}
 
-
-	//临时复制?
+	// 初始化 Logitech 驱动
 	Send::Error Logitech::create() {
 		return driver.create();
 	}
 
-	//临时复制?
+	// 销毁 Logitech 驱动
 	void Logitech::destroy() {
 		driver.destroy();
 	}
 
-	//临时复制?+修复
+	// 发送一组鼠标输入事件
 	uint32_t Logitech::send_mouse_input(const INPUT inputs[], uint32_t n) {
 		uint32_t count = 0;
 		for (uint32_t i = 0; i < n; ++i) {
@@ -30,22 +29,24 @@ namespace Send::Type::Internal {
 		return count;
 	}
 
-	//临时复制?
+	// 发送单个鼠标输入事件
 	bool Logitech::send_mouse_input(const MOUSEINPUT& mi) {
 		return send_mouse_report<MouseReport>(mi);
 	}
 
-
-	//临时复制?
+	// 发送键盘输入事件（支持修饰键状态更新）
 	bool Logitech::send_keyboard_input(const KEYBDINPUT& ki) {
 		std::lock_guard lock(keyboard_mutex);
 
+		// 判断是按下还是释放
 		bool keydown = !(ki.dwFlags & KEYEVENTF_KEYUP);
 		if (is_modifier(ki.wVk)) {
+			// 修饰键状态更新
 			set_modifier_state(ki.wVk, keydown);
 		}
 		else {
-			uint8_t usage = Usb::keyboard_vk_to_usage((uint8_t)ki.wVk);;
+			// 普通键处理：按下则填入 report，抬起则清除
+			uint8_t usage = Usb::keyboard_vk_to_usage((uint8_t)ki.wVk);
 			if (keydown) {
 				for (int i = 0; i < 6; i++) {
 					if (keyboard_report.keys[i] == 0) {
@@ -53,37 +54,35 @@ namespace Send::Type::Internal {
 						break;
 					}
 				}
-				//full
+				// #TODO: 满键处理
 			}
 			else {
 				for (int i = 0; i < 6; i++) {
 					if (keyboard_report.keys[i] == usage) {
 						keyboard_report.keys[i] = 0;
-						//#TODO: move to left?
+						// #TODO: 是否需要左移键位
 						break;
 					}
 				}
 			}
 		}
 
+		// 提交键盘报告
 		return driver.report_keyboard(keyboard_report);
 	}
 
-	//临时复制?
+	// 发送鼠标报告（支持移动、滚轮、按键等事件）
 	template <class ReportType>
-	bool Logitech::send_mouse_report(const MOUSEINPUT& mi)
-	{
+	bool Logitech::send_mouse_report(const MOUSEINPUT& mi) {
 		std::lock_guard lock(mouse_mutex);
-
 
 		ReportType mouse_report{};
 
-		//#TODO: move and then click, or click and then move? former?
-
-		//#TODO: MOUSEEVENTF_MOVE_NOCOALESCE
+		// 处理鼠标移动
 		if (mi.dwFlags & MOUSEEVENTF_MOVE) {
 			POINT move{ mi.dx, mi.dy };
 			if (mi.dwFlags & MOUSEEVENTF_ABSOLUTE) {
+				// 坐标转换：绝对 -> 屏幕坐标 -> 相对坐标
 				if (mi.dwFlags & MOUSEEVENTF_VIRTUALDESK)
 					mouse_virtual_desk_absolute_to_screen(move);
 				else
@@ -91,6 +90,7 @@ namespace Send::Type::Internal {
 				mouse_screen_to_relative(move);
 			}
 
+			// 分段处理大移动量，避免超出坐标类型范围
 			static_assert(std::is_same_v<decltype(mouse_report.x), decltype(mouse_report.y)>);
 			using CoordinatesType = decltype(mouse_report.x);
 			constexpr auto maxValue = max_value<CoordinatesType>();
@@ -112,6 +112,7 @@ namespace Send::Type::Internal {
 					mouse_report.y = 0;
 				}
 
+				// 分段发送移动报告
 				driver.report_mouse(mouse_report, compensate_switch = -compensate_switch);
 			}
 
@@ -123,18 +124,22 @@ namespace Send::Type::Internal {
 			mouse_report.y = 0;
 		}
 
-		if (mi.dwFlags & MOUSEEVENTF_WHEEL) { // TODO MOUSEEVENTF_HWHEEL
+		// 处理滚轮事件
+		if (mi.dwFlags & MOUSEEVENTF_WHEEL) {
 			mouse_report.wheel = std::bit_cast<int32_t>(mi.mouseData) > 0 ? 1 : -1;
 		}
 
+		// 处理左右中按键
 #define CODE_GENERATE(down, up, member)  \
             if (mi.dwFlags & down || mi.dwFlags & up)  \
                 mouse_report.button.##member = mi.dwFlags & down;
 
-		CODE_GENERATE(MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, LButton)  //#TODO: may be switched?
+		CODE_GENERATE(MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, LButton)
 			CODE_GENERATE(MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP, RButton)
 			CODE_GENERATE(MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_MIDDLEUP, MButton)
 #undef CODE_GENERATE
+
+			// 处理 X 按钮
 			if (mi.dwFlags & (MOUSEEVENTF_XDOWN | MOUSEEVENTF_XUP)) {
 				bool down = mi.dwFlags & MOUSEEVENTF_XDOWN;
 				switch (mi.mouseData) {
@@ -143,9 +148,8 @@ namespace Send::Type::Internal {
 				}
 			}
 
+		// 提交鼠标报告
 		return driver.report_mouse(mouse_report, compensate_switch = -compensate_switch);
 	}
 
-} // namespace Send::Type::Internal
-
-
+}
