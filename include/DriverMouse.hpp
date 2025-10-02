7376d5d0be6cc164a24ed522c3234b49fd18e5a8
zhihao.zhou <1559152947@qq.com>
2025-10-02 20:16:47 +0800

    删除头文件

--- include/DriverMouse.hpp
+++ include/DriverMouse.hpp
@@ -4,51 +4,41 @@
 
 
 
-//使用C风格
-//#ifdef __cplusplus
-//extern "C" {
-//#endif
+// -------------------- 初始化/销毁 IbSendBase.hpp --------------------
 
-	// -------------------- 初始化/销毁 --------------------
-	
-	// 初始化发送模块
-	DLLAPI Send::Error WINAPI IbSendInit(Send::SendType type, Send::InitFlags flags, void* argument);
+// 初始化发送模块
+DLLAPI Send::Error WINAPI IbSendInit(Send::SendType type, Send::InitFlags flags, void* argument);
 
-	// 销毁发送模块
-	DLLAPI void WINAPI IbSendDestroy();
+// 销毁发送模块
+DLLAPI void WINAPI IbSendDestroy();
 
-	// 同步键盘状态
-	DLLAPI void WINAPI IbSendSyncKeyStates();
+// 同步键盘状态
+DLLAPI void WINAPI IbSendSyncKeyStates();
 
-	// -------------------- 鼠标控制 --------------------
+// -------------------- 鼠标控制 IbSendMouse.hpp --------------------
 
-	// 移动鼠标
-	DLLAPI bool WINAPI IbSendMouseMove(uint32_t x, uint32_t y, Send::MoveMode mode);
+// 移动鼠标
+DLLAPI bool WINAPI IbSendMouseMove(uint32_t x, uint32_t y, Send::MoveMode mode);
 
-	// 鼠标点击
-	DLLAPI bool WINAPI IbSendMouseClick(Send::MouseButton button);
+// 鼠标点击
+DLLAPI bool WINAPI IbSendMouseClick(Send::MouseButton button);
 
-	// 鼠标滚轮
-	DLLAPI bool WINAPI IbSendMouseWheel(int32_t movement);
+// 鼠标滚轮
+DLLAPI bool WINAPI IbSendMouseWheel(int32_t movement);
 
-	// -------------------- 键盘控制 --------------------
-	struct KeyboardModifiers;
+// -------------------- 键盘控制 IbSendKeyboard.hpp --------------------
+struct KeyboardModifiers;
 
-	// 按下指定键
-	DLLAPI bool WINAPI IbSendKeybdDown(uint16_t vk);
+// 按下指定键
+DLLAPI bool WINAPI IbSendKeybdDown(uint16_t vk);
 
-	// 释放指定键
-	DLLAPI bool WINAPI IbSendKeybdUp(uint16_t vk);
+// 释放指定键
+DLLAPI bool WINAPI IbSendKeybdUp(uint16_t vk);
 
-	// 按下并释放指定键，可附加修饰键
-	DLLAPI bool WINAPI IbSendKeybdDownUp(uint16_t vk, KeyboardModifiers modifiers);
+// 按下并释放指定键，可附加修饰键
+DLLAPI bool WINAPI IbSendKeybdDownUp(uint16_t vk, KeyboardModifiers modifiers);
 
 
-
-//
-//#ifdef __cplusplus
-//}
-//#endif
 
 
 
